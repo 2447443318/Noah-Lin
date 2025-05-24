6c05b550ec174c6d89172fea64684627d66bc0ea
2447443318 <qlin0062@uni.sydney.edu.au>
2025-05-24 22:00:21 +1000

    Create order.py

---
+++ order.py
@@ -0,0 +1,10 @@
+class Order:
+    def __init__(self, num:int, price:int, is_buy):
+        self.num = num
+        self.price = price
+        self.is_buy = is_buy
+
+    def __str__(self):
+        if self.is_buy:
+            return f'Order:buy {self.num} stocks on price: ${self.price}\n'
+        return f'Order:sell {self.num} stocks on price: ${self.price}\n' 
