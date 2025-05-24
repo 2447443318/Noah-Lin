e20bc3c5ae12d31ebb3ad5fb70e5f9c463b11123
2447443318 <qlin0062@uni.sydney.edu.au>
2025-05-24 22:00:59 +1000

    Create market.py

---
+++ market.py
@@ -0,0 +1,56 @@
+from order import Order
+from market import Market
+
+import random
+from time import sleep
+
+print("Welcom to Stock Market\n")
+is_code = False
+while not is_code:
+    name = input("Please input trading code(three intigers) so we can show the transaction:")
+    if len(name) > 3 or len(name) == 0:
+        print('Trading code should be three intigers!')
+        continue
+    for letter in name:
+        if letter not in '0123456789':
+            print('Code should not contain letters!')
+            is_code = False
+            break
+        is_code = True
+
+ask_price = sorted(random.sample(range(30,50),5),reverse= True)
+ask_number = sorted(random.sample(range(100,200),5))
+
+bid_price = sorted(random.sample(range(1,25),5),reverse= True)
+bid_number = sorted(random.sample(range(100,200),5))
+
+market = Market(name, ask_price, ask_number, bid_price, bid_number)
+
+print("Loading......")
+sleep(0.5)
+print("......\n\n")
+sleep(0.5)
+
+market.show()
+print()
+is_quit = False
+i = 1
+
+while not is_quit:
+    a = input(f"[{i}]please input your order(sell/buy price number): ").split()
+    i += 1
+    if a == ['quit']:
+        is_quit = True
+    elif len(a) !=3 or a[0] not in ['quit', 'buy','sell'] or int(a[1]) == 0 or int(a[1]) < 0 or int(a[2]) == 0:
+        print('Invalid order!')
+        continue
+    else:    
+        buy, price, number = a
+        if buy == "buy":
+            is_buy = True
+        elif buy == "sell":
+            is_buy = False  
+        order = Order(int(number), int(price), is_buy)
+        market.order_dealing(order)
+
+print('\nThank you for using our trading system!')
